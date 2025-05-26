bc80ce59b9487f8a9c62716c4326a3a44f034103
Tristurier <tristan.couturier@etu.estia.fr>
2025-05-26 11:55:09 +0200

    merge branch test1

--- src/features.h
+++ src/features.h
@@ -6,4 +6,5 @@ void helloWorld();
 
 void dimension(char *source_path);
 
+void first_pixel (char *source_path);
 #endif
--- src/main.c
+++ src/main.c
@@ -31,6 +31,10 @@ int main(int argc, char **argv) {
   if ( strncmp( configuration.command, "dimension", 9 ) == 0 ) {
     dimension( configuration.filenames[0] );
 }
+
+if ( strncmp( configuration.command, "first_pixel", 11 ) == 0 ) {
+    first_pixel( configuration.filenames[0] );
+}
   /*
    * TO COMPLETE
    */
